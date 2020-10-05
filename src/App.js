4ce6df7d26b553f2181e1eab73aeec2ce2cd2083
dmarselus <37393543+dmarselus@users.noreply.github.com>
2020-10-05 14:38:01 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,103 +1,143 @@
-import React, { useState } from "react";
+// Simple Roomba Challenge
+// Generate button => takes file input to generate the output
+// Reset button => reset files and so that user can reupload new input file
+
+//Improvement
+// table styling, UX, and logic.
+
+import React, { useState, useEffect } from "react";
 import "./styles.css";
-import { Button, Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Files from "react-files";
+
+let reader = new FileReader();
+let counter = 0;
 export default function App() {
-  const [roomba, setRoomba] = useState([1, 1]);
-  const [width, setWidth] = useState(5);
-  const [height, setHeight] = useState("");
-  const [dirt, setDirt] = useState([]);
+  const [roomba, setRoomba] = useState([1, 1]); //roomba Location
+  const [input, setInput] = useState({}); //file input
   const [output, setOutput] = useState({
     step: [1],
     location: [roomba],
     action: ["-"],
     totalDirt: [0],
     totalWall: [0]
-  });
-  let ins = [
-    "N",
-    "E",
-    "E",
-    "N",
-    "N",
-    "N",
-    "E",
-    "E",
-    "S",
-    "W",
-    "S",
-    "S",
-    "S",
-    "S",
-    "S"
-  ];
-  let step = [];
-  let posHist = [roomba];
-
-  let dirtLocations = [
-    [1, 2],
-    [3, 5],
-    [5, 5],
-    [7, 9]
-  ];
+  }); //output object to be mapped
+  const [uploadToggle, setUploadToggle] = useState(true); //toggling uploading to trigger useEffect
+  const [uploadMessage, setUploadMessage] = useState(""); //display message according to the input
+  useEffect(() => {
+    reader.onload = (event) => {
+      setInput(JSON.parse(event.target.result));
+    };
+  }, [uploadToggle]);
 
   function handleSubmit() {
-    let outputObj = { ...output };
-    let obj = {
-      N: [0, 1],
-      S: [0, -1],
-      W: [-1, 0],
-      E: [1, 0]
-    };
-    let temp = [...roomba];
-    let totalWallHitCounter = 0;
-    let dirtCounter = 0;
-    ins.forEach((direction, index) => {
-      let newLocX = temp[0] + obj[direction][0];
-      let newLocY = temp[1] + obj[direction][1];
-      outputObj.step.push(index + 2);
-      outputObj.action.push(direction);
-      //todo add max w and h
-      if (newLocX >= 0 && newLocY >= 0) {
-        //no hit
-        temp = [temp[0] + obj[direction][0], temp[1] + obj[direction][1]];
-        outputObj.location.push(temp);
-        if (JSON.stringify(dirtLocations).includes(JSON.stringify(temp)))
-          dirtCounter++;
-      } else {
-        totalWallHitCounter++;
-        outputObj.location.push([temp[0], temp[1]]);
-      }
-      outputObj.totalWall.push(totalWallHitCounter);
-      outputObj.totalDirt.push(dirtCounter);
-    });
-    setRoomba(temp);
-    setOutput(outputObj);
-    console.log(outputObj);
+    function validInput(obj) {
+      let validKeys = [
+        "roomDimensions",
+        "initialRoombaLocation",
+        "dirtLocations",
+        "drivingInstructions"
+      ];
+      let isValid = false;
+      Object.keys(obj).map((key) => {
+        if (validKeys.includes(key)) isValid = true;
+        else isValid = false;
+      });
+      setUploadMessage(isValid ? "Success" : "File doesnt match");
+
+      return isValid;
+    }
+    if (validInput(input)) {
+      let {
+        roomDimensions,
+        initialRoombaLocation,
+        dirtLocations,
+        drivingInstructions
+      } = input;
+      setRoomba(initialRoombaLocation);
+      let outputObj = { ...output };
+      let obj = {
+        N: [0, 1],
+        S: [0, -1],
+        W: [-1, 0],
+        E: [1, 0]
+      };
+      let temp = [...roomba];
+      let totalWallHitCounter = 0;
+      let dirtCounter = 0;
+      drivingInstructions.forEach((direction, index) => {
+        let newLocX = temp[0] + obj[direction][0];
+        let newLocY = temp[1] + obj[direction][1];
+        outputObj.step.push(index + 2);
+        outputObj.action.push(direction);
+        //todo add max w and h
+        if (
+          newLocX >= 0 &&
+          newLocX <= roomDimensions[0] &&
+          newLocY >= 0 &&
+          newLocY <= roomDimensions[1]
+        ) {
+          //no hit
+          temp = [temp[0] + obj[direction][0], temp[1] + obj[direction][1]];
+          outputObj.location.push(temp);
+          if (JSON.stringify(dirtLocations).includes(JSON.stringify(temp)))
+            dirtCounter++;
+        } else {
+          totalWallHitCounter++;
+          outputObj.location.push([temp[0], temp[1]]);
+        }
+        outputObj.totalWall.push(totalWallHitCounter);
+        outputObj.totalDirt.push(dirtCounter);
+      });
+      setRoomba(temp);
+      setOutput(outputObj);
+    }
   }
 
   function renderInputs() {
     return (
       <>
-        <div>
-          <label for="exampleFormControlInput1">Width: </label>
-          <input type="number" placeholder="0" value={width} />
-        </div>
-        <div>
-          <label for="exampleFormControlInput1">Height: </label>
-          <input type="number" placeholder="0" value={height} />
-        </div>
-        <div>
-          <label for="exampleFormControlInput1">Dirt X: </label>
-          <input type="number" placeholder="0" value={""} />
-        </div>
-        <div>
-          <label for="exampleFormControlInput1">Dirt Y: </label>
-          <input type="number" placeholder="0" value={""} />
-        </div>
-        <Button class="btn btn-secondary" onClick={handleSubmit}>
-          Submit
+        <Files
+          className="files-dropzone"
+          onChange={(file) => {
+            setUploadToggle(!uploadToggle);
+            reader.readAsText(file[counter]);
+            counter++;
+          }}
+          onError={(err) => console.log(err)}
+          accepts={[".json"]}
+          clickable
+        >
+          Drop files here or click to upload
+        </Files>
+        <Button
+          class="btn btn-secondary"
+          style={{ margin: "10px 10px 10px 10px" }}
+          onClick={handleSubmit}
+        >
+          Generate
         </Button>
+        <Button
+          class="btn btn-warning"
+          style={{ margin: "10px 10px 10px 10px" }}
+          onClick={() => {
+            setInput({});
+            setUploadMessage("");
+            setUploadToggle(!uploadToggle);
+            setRoomba([1, 1]);
+            setOutput({
+              step: [1],
+              location: [roomba],
+              action: ["-"],
+              totalDirt: [0],
+              totalWall: [0]
+            });
+          }}
+        >
+          Reset
+        </Button>
+        <h6>{uploadMessage}</h6>
       </>
     );
   }
@@ -107,14 +147,12 @@ export default function App() {
       <div
         style={{
           width: "100%",
-          backgroundColor: "red",
           display: "flex",
           justifySelf: "center",
           margin: "10px 0 10px 0"
         }}
       >
         {Object.entries(objToRender).map(([key, val]) => {
-          console.log(key, val);
           return (
             <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
               <div style={{ borderStyle: "solid" }}>
@@ -134,29 +172,10 @@ export default function App() {
     );
   }
 
-  function handleFileSelected(e: React.ChangeEvent<HTMLInputElement>): void {
-    const files = Array.from(e.target.files);
-    console.log("files:", files);
-  }
-
   return (
     <div className="App">
       {renderInputs()}
-      <Form.File
-        className="position-relative"
-        required
-        name="file"
-        label="File"
-        onChange={handleFileSelected}
-        // isInvalid={!!errors.file}
-        // feedback={errors.file}
-        id="validationFormik107"
-        feedbackTooltip
-      />
       {renderOutput(output)}
-      <h1>
-        {roomba[0]},{roomba[1]}
-      </h1>
     </div>
   );
 }
